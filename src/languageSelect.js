710e9fb91b856d7a9008f8d7f775b42311828522
Ravi Usadadiya <80156957+ravi-3k96@users.noreply.github.com>
2021-04-10 14:01:38 +0530

    Update languageSelect.js

--- src/languageSelect.js
+++ src/languageSelect.js
@@ -1,33 +1,60 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import Select from "react-select";
 
 const LanguageSelect = () => {
 const { i18n } = useTranslation()
-const i18nLanguage = i18n.language
-const [currentI18nLanguage, setCurrentI18nLanguage] = useState(i18nLanguage)
+//const i18nLanguage = i18n.language
+const currentLanguageObj = JSON.parse(localStorage.getItem('LanguageObj')) ? JSON.parse(localStorage.getItem('LanguageObj')) : localStorage.setItem('LanguageObj',JSON.stringify({"name":"English","code":"en"}))
+const [currentI18nLanguage, setCurrentI18nLanguage] = useState(currentLanguageObj)
 
-const languageOptions = {
-    en: 'English',
-    fr: 'Français',
-    de: 'Deutsch',
-    es: 'Español'
-   
+console.log(localStorage.getItem('LanguageObj'));
+const languageOptions = [
+  {
+    "name": "Deutsch",
+    "code": "de"
+  },
+  {
+    "name": "Español",
+    "code": "es"
+  },
+  {
+    "name": "English",
+    "code": "en"
+  },
+  {
+    "name": "Français",
+    "code": "fr"
+  }];
+
+const changeLanguage = (obj) => {
+  i18n.changeLanguage(obj.code)
+  setCurrentI18nLanguage(obj);
+  localStorage.setItem('LanguageObj',JSON.stringify(obj));
 };
-
-const changeLanguage = (language) => {
-  i18n.changeLanguage(language)
-  setCurrentI18nLanguage(language)
-}
+// const changeLanguage = (language) => {
+//   i18n.changeLanguage(language)
+//   setCurrentI18nLanguage(language)
+// }
 
 return (
-    <select 
-      onChange={(event) => changeLanguage(event.target.value)}
+    // <select 
+    //   onChange={(event) => changeLanguage(event.target.value)}
+    //   value={currentI18nLanguage}
+    // >
+    // {Object.entries(languageOptions).map(([id, name]) => (
+    //     <option key={id} value={id}>{name}</option>
+    //  ))}
+    // </select>
+
+    <Select
+      placeholder="Select Language"
       value={currentI18nLanguage}
-    >
-    {Object.entries(languageOptions).map(([id, name]) => (
-        <option key={id} value={id}>{name}</option>
-     ))}
-    </select>
+      options={languageOptions}
+      onChange={changeLanguage}
+      getOptionLabel={x => x.name}
+      getOptionValue={x => x.code}
+    />
 )
 }
 export default LanguageSelect;
